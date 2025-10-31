1f47995a278bfa8c6babd999dd58da76f24343b4
Josh <joshrachc@icloud.com>
2025-10-31 13:47:22 -0400

    Refactor baselist command to use subcommands

--- src/commands/baselist.js
+++ src/commands/baselist.js
@@ -1,36 +1,24 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
 import { loadState, saveState } from '../stateStore.js';
 import { getAirportByIata } from '../airlineClient.js';
 
-export const data = new SlashCommandBuilder()
-    .setName('baselist')
-    .setDescription('Manage your list of base airports')
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('add')
-            .setDescription('Add a base airport by its IATA code')
-            .addStringOption(option => 
-                option.setName('iata')
-                .setDescription('The 3-letter IATA code (e.g., IST)')
-                .setRequired(true)))
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('delete')
-            .setDescription('Remove a base airport by its IATA code')
-            .addStringOption(option => 
-                option.setName('iata')
-                .setDescription('The 3-letter IATA code (e.g., IST)')
-                .setRequired(true)))
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('view')
-            .setDescription('View all airports currently in your baselist'));
+export const subcommands = (builder) =>
+    builder.addSubcommand(sub => sub
+        .setName('baselist_add')
+        .setDescription('Add a base airport by its IATA code')
+        .addStringOption(opt => opt.setName('iata').setDescription('The 3-letter IATA code (e.g., IST)').setRequired(true)))
+    .addSubcommand(sub => sub
+        .setName('baselist_delete')
+        .setDescription('Remove a base airport by its IATA code')
+        .addStringOption(opt => opt.setName('iata').setDescription('The 3-letter IATA code (e.g., IST)').setRequired(true)))
+    .addSubcommand(sub => sub
+        .setName('baselist_view')
+        .setDescription('View all airports currently in your baselist'));
 
 export async function execute(interaction) {
     const subcommand = interaction.options.getSubcommand();
     const state = await loadState();
 
-    if (subcommand === 'view') {
+    if (subcommand === 'baselist_view') {
         const iatas = Object.keys(state.baseAirports);
         if (iatas.length === 0) {
             return interaction.reply({ content: 'Your baselist is currently empty.', ephemeral: true });
@@ -40,7 +28,7 @@ export async function execute(interaction) {
         
         return interaction.reply({ content: `**Current Baselist:**\n${baseListString}`, ephemeral: true });
     
-    } else if (subcommand === 'add') {
+    } else if (subcommand === 'baselist_add') {
         await interaction.deferReply({ ephemeral: true });
         const iata = interaction.options.getString('iata').toUpperCase();
         
@@ -54,7 +42,6 @@ export async function execute(interaction) {
                 return interaction.editReply(`Could not find an airport with IATA code ${iata}.`);
             }
             
-            // Add to state, mapping IATA -> airportId
             state.baseAirports[iata] = airport.id;
             await saveState(state);
             
@@ -65,7 +52,7 @@ export async function execute(interaction) {
             return interaction.editReply('An error occurred while trying to find that airport.');
         }
 
-    } else if (subcommand === 'delete') {
+    } else if (subcommand === 'baselist_delete') {
         const iata = interaction.options.getString('iata').toUpperCase();
 
         if (!state.baseAirports[iata]) {
